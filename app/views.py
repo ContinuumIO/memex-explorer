0bd5bba98c8ed09b08001430aa5b977701c4218f
Brittain Hard <brittainhard@gmail.com>
2014-12-17 09:32:04 -0600

    Adding more specific namespaces to views.

--- app/views.py
+++ app/views.py
@@ -29,9 +29,10 @@ from bokeh.plotting import ColumnDataSource
 # -------------
 
 from . import app, db
-from .models import *
-from .db_api import *
-from .forms import *
+from app import models
+from app import db_api
+from app import forms
+from app import crawls
 
 from .rest_api import api
 from .mail import send_email
@@ -41,7 +42,6 @@ from .config import ADMINS, DEFAULT_MAIL_SENDER, BASEDIR, SEED_FILES, \
 
 from .auth import requires_auth
 from .plotting import plot_builder
-from .crawls import AcheCrawl, NutchCrawl
 
 
 from .viz.domain import Domain
@@ -64,24 +64,26 @@ def context():
 
     if request.view_args and 'project_name' in request.view_args:
         project_name = request.view_args['project_name']
-        project = get_project(project_name)
+        project = db_api.get_project(project_name)
         if not project:
             return {}
             # flash("Project '%s' was not found." % project_name, 'error')
             # abort(404)
 
-        crawls = get_crawls(project.id)
-        dashboards = get_dashboards(project.id)
-        models = get_models()
-        images = get_image_space(project.id)
+        crawls = db_api.get_crawls(project.id)
+        dashboards = db_api.get_dashboards(project.id)
+        model = db_api.get_models()
+        images = db_api.get_image_space(project.id)
+
 
 
         context_vars.update(dict(
             project=project, crawls=crawls, dashboards=dashboards, \
-            models=models, images=images))
-
+            models=model, images=images))
     # All pages should (potentially) be able to present all projects
-    context_vars.update(projects=Project.query.all())
+    import pdb; pdb.set_trace;
+    projects = models.Project.query.all()
+    context_vars.update(projects=projects)
 
     return context_vars
 
@@ -128,10 +130,10 @@ def project(project_name):
 
 @app.route('/add_project', methods=['GET', 'POST'])
 def add_project():
-    form = ProjectForm(request.form)
+    form = forms.ProjectForm(request.form)
 
     if form.validate_on_submit():
-        project = Project(name=form.name.data, description=form.description.data, \
+        project = models.Project(name=form.name.data, description=form.description.data, \
                         icon=form.icon.data)
         db.session.add(project)
         db.session.commit()
@@ -146,26 +148,26 @@ def add_project():
 
 @app.route('/<project_name>/add_crawl', methods=['GET', 'POST'])
 def add_crawl(project_name):
-    form = CrawlForm()
+    form = forms.CrawlForm()
     if form.validate_on_submit():
         seed_filename = secure_filename(form.seeds_list.data.filename)
         form.seeds_list.data.save(SEED_FILES + seed_filename)
         # TODO allow upload configuration
         #config_filename = secure_filename(form.config.data.filename)
         #form.config.data.save(CONFIG_FILES + config_filename)
-        project = get_project(project_name)
-        crawl = db_add_crawl(project, form, seed_filename)
+        project = db_api.get_project(project_name)
+        crawl = db_api.db_add_crawl(project, form, seed_filename)
         subprocess.Popen(['mkdir', os.path.join(CRAWLS_PATH, crawl.name)])
 
         if crawl.crawler == 'ache':
-            db_init_ache(project, crawl)
+            db_api.db_init_ache(project, crawl)
 
         else: 
             #TODO add db_init_nutch
             pass
 
         flash('%s has successfully been registered!' % form.name.data, 'success')
-        return redirect(url_for('crawl', project_name=get_project(project_name),
+        return redirect(url_for('crawl', project_name=db_api.get_project(project_name),
                                          crawl_name=form.name.data))
 
     return render_template('add_crawl.html', form=form)
@@ -173,9 +175,9 @@ def add_crawl(project_name):
 
 @app.route('/<project_name>/add_model', methods=['GET', 'POST'])
 def add_model(project_name):
-    form = DataModelForm()
+    form = forms.DataModelForm()
     if form.validate_on_submit():
-        registered_model = DataModel.query.filter_by(name=form.name.data).first()
+        registered_model = forms.DataModel.query.filter_by(name=form.name.data).first()
         if registered_model:
             flash('Data model name already exists, please choose another name', 'error')
             return render_template('add_data_model.html', form=form)
@@ -184,13 +186,13 @@ def add_model(project_name):
             os.makedirs(MODEL_FILES + form.name.data)
         for x in files:
             x.save(MODEL_FILES + form.name.data + '/' + x.filename)
-        model = DataModel(name=form.name.data,
+        model = models.DataModel(name=form.name.data,
                           filename=MODEL_FILES + form.name.data)
         db.session.add(model)
         db.session.commit()
         flash('Model has successfully been registered!', 'success')
         return redirect(url_for('project', 
-                                project_name=get_project(project_name).name))
+                                project_name=db_api.get_project(project_name).name))
 
     return render_template('add_data_model.html', form=form)
 
@@ -202,8 +204,8 @@ def crawls(project_name):
 
 @app.route('/<project_name>/crawls/<crawl_name>')
 def crawl(project_name, crawl_name):
-    project = get_project(project_name)
-    crawl = get_crawl(crawl_name)
+    project = db_api.get_project(project_name)
+    crawl = db_api.get_crawl(crawl_name)
 
     if not project:
         flash("Project '%s' was not found." % project_name, 'error')
@@ -221,17 +223,17 @@ def run_crawl(project_name, crawl_name):
     if CRAWLS_RUNNING.has_key(key):
         return "Crawl is already running."
     else:
-        crawl = get_crawl(crawl_name)
+        crawl = db_api.get_crawl(crawl_name)
         seeds_list = crawl.seeds_list
         if crawl.crawler=="ache":
-            model = get_crawl_model(crawl)
-            crawl_instance = AcheCrawl(crawl_name=crawl.name, seeds_file=seeds_list, model_name=model.name,
+            model = db_api.get_crawl_model(crawl)
+            crawl_instance = crawls.AcheCrawl(crawl_name=crawl.name, seeds_file=seeds_list, model_name=model.name,
                                        conf_name=crawl.config)
             pid = crawl_instance.start()
             CRAWLS_RUNNING[key] = crawl_instance
             return "Crawl %s running" % crawl.name
         elif crawl.crawler=="nutch":
-            crawl_instance = NutchCrawl(seed_dir=seeds_list, crawl_dir=crawl.name)
+            crawl_instance = crawls.NutchCrawl(seed_dir=seeds_list, crawl_dir=crawl.name)
             pid = crawl_instance.start()
             CRAWLS_RUNNING[key] = crawl_instance
             return "Crawl %s running" % crawl.name
@@ -254,7 +256,7 @@ def stop_crawl(project_name, crawl_name):
 @app.route('/<project_name>/crawls/<crawl_name>/refresh', methods=['POST'])
 def refresh(project_name, crawl_name):
 
-    domain_plot = Plot.query.filter_by(name='domain').first()
+    domain_plot = models.Plot.query.filter_by(name='domain').first()
 
     # TODO retrieve data from db. These are only valid if crawler==ache.
     crawled_data_uri = os.path.join(CRAWLS_PATH, crawl_name, 'data/data_monitor/crawledpages.csv')
@@ -265,7 +267,7 @@ def refresh(project_name, crawl_name):
     domain = Domain(domain_sources, domain_plot)
     domain.push_to_server()
 
-    harvest_plot = Plot.query.filter_by(name='harvest').first()
+    harvest_plot = models.Plot.query.filter_by(name='harvest').first()
 
     harvest_data_uri = os.path.join(CRAWLS_PATH, crawl_name, 'data/data_monitor/harvestinfo.csv')
     harvest_sources = dict(harvest=harvest_data_uri)
@@ -279,12 +281,12 @@ def refresh(project_name, crawl_name):
 @app.route('/<project_name>/crawls/<crawl_name>/dashboard')
 def view_plots(project_name, crawl_name):
 
-    crawl = Crawl.query.filter_by(name=crawl_name).first()
+    crawl = models.Crawl.query.filter_by(name=crawl_name).first()
 
     key = project_name + '-' + crawl_name
 
     # Domain
-    plot = Plot.query.filter_by(name=key + '-' + 'domain').first()
+    plot = models.Plot.query.filter_by(name=key + '-' + 'domain').first()
 
     #TODO use db_api
     crawled_data_uri = os.path.join(CRAWLS_PATH, crawl_name, 'data/data_monitor/crawledpages.csv')
@@ -301,7 +303,7 @@ def view_plots(project_name, crawl_name):
 
     # Harvest
 
-    plot = Plot.query.filter_by(name='harvest').first()
+    plot = models.Plot.query.filter_by(name='harvest').first()
 
     harvest_data_uri = os.path.join(CRAWLS_PATH, crawl_name, 'data/data_monitor/harvestinfo.csv')
 
@@ -334,10 +336,6 @@ def status_crawl(project_name, crawl_name):
 
 @app.route('/<project_name>/crawls/<crawl_name>/image_space')
 def image_space(project_name, crawl_name):
-    project = Project.query.filter_by(name=project_name).first()
-    crawl = Crawl.query.filter_by(name=crawl_name).first()
-    crawls = Crawl.query.filter_by(project_id=project.id)
-    dashboards = Dashboard.query.filter_by(project_id=project.id)
     if not crawl:
         flash("Crawl '%s' was not found." % crawl_name, 'error')
         abort(404)
@@ -347,8 +345,7 @@ def image_space(project_name, crawl_name):
     elif crawl.project_id != project.id:
         flash("This crawl is not part of project '%s'" % project_name, 'error')
         abort(404)
-    return render_template('crawl.html', project=project, crawl=crawl,\
-                            crawls=crawls, dashboards=dashboards)
+    return render_template('crawl.html')
 
 
 # Data
@@ -434,9 +431,6 @@ def data_explore(crawl_endpoint, data_endpoint):
 
 @app.route('/<project_name>/add_dashboard', methods=['GET', 'POST'])
 def add_dashboard(project_name):
-    project = Project.query.filter_by(name=project_name).first()
-    crawls = Crawl.query.filter_by(project_id=project.id)
-    dashboards = Dashboard.query.filter_by(project_id=project.id)
     form = DashboardForm(request.form)
 
     if form.validate_on_submit():
@@ -448,17 +442,13 @@ def add_dashboard(project_name):
         return redirect(url_for('dash', project_name=project.name, \
                         dashboard_name=form.name.data))
 
-    return render_template('add_dashboard.html', project=project, crawls=crawls,
-                            form=form, dashboards=dashboards)
+    return render_template('add_dashboard.html')
 
 
 @app.route('/<project_name>/dashboards/<dashboard_name>')
 def dash(project_name, dashboard_name):
-    project = Project.query.filter_by(name=project_name).first()
-    crawls = Crawl.query.filter_by(project_id=project.id)
-    dashboards = Dashboard.query.filter_by(project_id=project.id)
-    dashboard = Dashboard.query.filter_by(name=dashboard_name).first()
-    plots = Plot.query.filter_by(project_id=project.id)
+    dashboard = models.Dashboard.query.filter_by(name=dashboard_name).first()
+    plots = models.Plot.query.filter_by(project_id=project.id)
     if not dashboard:
         flash("Dashboard '%s' was not found." % dashboard_name, 'error')
         abort(404)
@@ -469,8 +459,7 @@ def dash(project_name, dashboard_name):
         flash("Dashboard is not part of project '%s'." % project_name, 'error')
         abort(404)
 
-    return render_template('dash.html', project=project, crawls=crawls, \
-                           dashboards=dashboards, dashboard=dashboard, plots=plots)
+    return render_template('dash.html', dashboard=dashboard, plots=plots)
 
 
 @app.route('/<crawl_endpoint>/plot/<plot_endpoint>')
@@ -527,8 +516,8 @@ def data_edit(data_endpoint):
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    form = ContactForm(request.form)
-    crawls = Crawl.query.all()
+    form = form.ContactForm(request.form)
+    crawls = models.Crawl.query.all()
 
     if form.validate_on_submit():
         subject = ' -- '.join([form.issue.data, form.name.data])
@@ -548,10 +537,10 @@ def contact():
 @app.route('/<project_name>/image_space/<image_id>/compare/')
 def compare(project_name, image_id):
 
-    project = get_project(project_name)
-    images = get_images(project.id)
+    project = db_api.get_project(project_name)
+    images = db_api.get_images(project.id)
 
-    img = get_image(image_id)
+    img = db_api.get_image(image_id)
 
     exif_info = dict(zip(('EXIF_BodySerialNumber', 'EXIF_LensSerialNumber',
               'Image_BodySerialNumber', 'MakerNote_InternalSerialNumber',
@@ -567,7 +556,7 @@ def compare(project_name, image_id):
     # internal_matches = [(x.split('/static/')[-1], x.split('/')[-1])
     #                         for x in full_match_paths]
 
-    internal_matches = get_matches(project.id, img.id)
+    internal_matches = db_api.get_matches(project.id, img.id)
     for x in internal_matches:
         if (img.id, x.id) in app.MATCHES:
             x.match = "true"
@@ -597,7 +586,7 @@ def image_source(image_id):
 
 @app.route('/<project_name>/image_space/<image_id>')
 def inspect(project_name, image_id):
-    img = get_image(image_id)
+    img = db_api.get_image(image_id)
 
     exif_info = dict(zip(('EXIF_BodySerialNumber', 'EXIF_LensSerialNumber',
               'Image_BodySerialNumber', 'MakerNote_InternalSerialNumber',
