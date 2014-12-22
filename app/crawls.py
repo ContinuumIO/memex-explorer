6250c4a6db9b2e44030125237253eb9e69f4926e
Christine Doig <christine.doig.cardet@gmail.com>
2014-12-22 16:17:22 -0600

    adding dump images and stats to nutch crawl

--- app/crawls.py
+++ app/crawls.py
@@ -1,7 +1,8 @@
+from __future__ import absolute_import, division, print_function
 import subprocess
 import os
 
-from .config import SEED_FILES, MODEL_FILES, CONFIG_FILES, CRAWLS_PATH, LANG_DETECT_PATH
+from .config import SEED_FILES, MODEL_FILES, CONFIG_FILES, CRAWLS_PATH, LANG_DETECT_PATH, IMAGE_SPACE_PATH
 
 
 class AcheCrawl(object):
@@ -13,12 +14,16 @@ class AcheCrawl(object):
         self.model_dir = os.path.join(MODEL_FILES, model_name)
         self.crawl_dir = os.path.join(CRAWLS_PATH, crawl_name)
         self.proc = None
+        # TODO record start and stop variables
+        #self.start_timestamp
+        #self.stop_timestamp
 
     def start(self):
         with open(os.path.join(self.crawl_dir, 'stdout.txt'), 'w') as stdout:
             with open(os.path.join(self.crawl_dir,'stderr.txt'), 'w') as stderr:
                 self.proc = subprocess.Popen(['ache', 'startCrawl', self.crawl_dir, self.config, self.seeds_file,
                                           self.model_dir, LANG_DETECT_PATH], stdout=stdout, stderr=stderr)
+        self.proc.poll()
         return self.proc.pid
 
     def stop(self):
@@ -32,16 +37,17 @@ class AcheCrawl(object):
         elif self.proc.returncode is None:
             return "Running"
         elif self.proc.returncode < 0:
-            return "Stopped (Unused)"
+            return "Process was terminated by signal %s" % self.proc.returncode
         else:
-            return "An error occurred"
+            return "Process ended"
 
 
 class NutchCrawl(object):
 
     def __init__(self, seed_dir, crawl_dir):
-        self.seed_dir =  SEED_FILES + "/" + seed_dir
-        self.crawl_dir = CRAWLS_PATH + "/" + crawl_dir
+        self.seed_dir =  os.path.join(SEED_FILES, seed_dir)
+        self.crawl_dir = os.path.join(CRAWLS_PATH, crawl_dir)
+        self.img_dir = os.path.join(IMAGE_SPACE_PATH, crawl_dir)
         #TODO Switch from "2" to parameter.
         # For now let's set up number_of_rounds to 2.
         self.number_of_rounds = "2"
@@ -51,6 +57,7 @@ class NutchCrawl(object):
 
     def start(self):
         self.proc = subprocess.Popen(['crawl', self.seed_dir, self.crawl_dir, self.number_of_rounds])
+        self.proc.poll()
         return self.proc.pid
 
     def stop(self):
@@ -64,6 +71,26 @@ class NutchCrawl(object):
         elif self.proc.returncode is None:
             return "Running"
         elif self.proc.returncode < 0:
-            return "Stopped (Unused)"
+            return "Process was terminated by signal %s" % self.proc.returncode
         else:
-            return "An error occurred"
+            return "Process ended"
+
+    def dump_images(self):
+        if self.proc.returncode is None:
+            return "Crawl process is still running, can't dump images yet"
+        elif self.proc.returncode < 0:
+            return "Process was terminated by signal %s, data could had been corrupted" % self.proc.returncode
+        else:
+            with open(os.path.join(self.crawl_dir, 'img_stdout.txt'), 'w') as stdout:
+                with open(os.path.join(self.crawl_dir,'img_stderr.txt'), 'w') as stderr:
+                    img_dump_proc = subprocess.Popen(['nutch', 'dump', '-outputDir', self.img_dir, '-segment',
+                                  os.path.join(self.crawl_dir, 'segments'), '-mimetype', 'image/jpeg', 'image/png',
+                                  'text/html'], stdout=stdout, stderr=stderr)
+            return "Dumping images"
+
+    def stats(self):
+        with open(os.path.join(self.crawl_dir, 'stats_stdout.txt'), 'w') as stdout:
+            with open(os.path.join(self.crawl_dir,'stats_stderr.txt'), 'w') as stderr:
+                stats_proc = subprocess.Popen(['nutch', 'readdb', os.path.join(self.crawl_dir, 'crawldb'), '-stats'],
+                                              stdout=stdout, stderr=stderr)
+        return stats_proc.stdout
